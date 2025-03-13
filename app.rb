fdc92f6c0c3e171f6f5d45f11330ed7b3b9fa72e
Bella Rose Wilson <bwilson9893@gmail.com>
2025-03-13 19:06:41 +0000

    removed jokes part

--- app.rb
+++ app.rb
@@ -13,15 +13,15 @@ get '/generate_password' do
   @password = SecureRandom.base64(15).gsub(/[^a-zA-Z0-9!@#$%^&*]/, '')
 
   # API call to fetch a Yo Momma joke
-  joke_api_url = URI('https://yomomma-api.herokuapp.com/')
-  response = Net::HTTP.get(joke_api_url)
+  #joke_api_url = URI('https://yomomma-api.herokuapp.com/')
+  #response = Net::HTTP.get(joke_api_url)
 
   # Parse the response and extract the joke
-  begin
-    @joke = JSON.parse(response)['joke']
-  rescue
-    @joke = "Couldn't fetch a joke. Try again later."
-  end
+  # begin
+  #   @joke = JSON.parse(response)['joke']
+  # rescue
+  #   @joke = "Couldn't fetch a joke. Try again later."
+  # end
 
   erb :result
 end
