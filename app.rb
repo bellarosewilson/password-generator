805706d2dc997bfdd48a9689a7d57ebcb1f16714
Bella Rose Wilson <bwilson9893@gmail.com>
2025-03-13 18:05:56 +0000

    pass

--- app.rb
+++ app.rb
@@ -9,11 +9,19 @@ get '/' do
 end
 
 get '/generate_password' do
+  # Generate a 15-character password with special characters
   @password = SecureRandom.base64(15).gsub(/[^a-zA-Z0-9!@#$%^&*]/, '')
-  
-  joke_api_url = URI('https://api.yomomma.info/')
+
+  # API call to fetch a Yo Momma joke
+  joke_api_url = URI('https://yomomma-api.herokuapp.com/')
   response = Net::HTTP.get(joke_api_url)
-  @joke = JSON.parse(response)['joke'] rescue "Couldn't fetch a joke. Try again later."
+
+  # Parse the response and extract the joke
+  begin
+    @joke = JSON.parse(response)['joke']
+  rescue
+    @joke = "Couldn't fetch a joke. Try again later."
+  end
 
   erb :result
 end
